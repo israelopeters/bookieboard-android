07bb6a0dd8266bcbbfd0a73ebc29c0464597d04c
israelopeters <israelopeters@gmail.com>
2025-04-09 12:55:08 +0100

    Update user state

--- app/src/main/java/com/example/bookieboard/model/User.kt
+++ app/src/main/java/com/example/bookieboard/model/User.kt
@@ -1,10 +1,10 @@
 package com.example.bookieboard.model
 
 data class User(
-    val email: String,
-    val firstName: String,
-    val lastName: String,
-    val bookieScore: Int,
-    val bookieRank: UserRank,
-    val roles: List<Role>
+    val email: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val bookieScore: Int? = null,
+    val bookieRank: UserRank? = null,
+    val roles: List<Role>? = null
 )
--- app/src/main/java/com/example/bookieboard/service/UserUiState.kt
+++ app/src/main/java/com/example/bookieboard/service/UserUiState.kt
@@ -1,7 +1,45 @@
 package com.example.bookieboard.service
 
-sealed class UserUiState<out T> {
-    data object Loading : UserUiState<Nothing>()
-    data class Success<T>(val data: T) : UserUiState<T>()
-    data class Error(val message: String) : UserUiState<Nothing>()
+import com.example.bookieboard.model.User
+
+data class UserUiStateState(
+    val email: String? = null,
+    val password: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val authMode: AuthMode = AuthMode.SIGNED_OUT,
+    val userData: User? = User(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+) {
+    fun isEntryValid(): Boolean {
+        return email?.isNotEmpty() == true && password?.isNotEmpty() == true
+    }
+}
+
+data class UserCreationState(
+    val email: String? = null,
+    val password: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val signUpMode: SignUpMode = SignUpMode.INACTIVE,
+    val isLoading: Boolean = false,
+    val error: String? = null
+) {
+    fun isEntryValid(): Boolean {
+        return email?.isNotEmpty() == true &&
+                password?.isNotEmpty() == true &&
+                firstName?.isNotEmpty() == true &&
+                lastName?.isNotEmpty() == true
+    }
+}
+
+enum class AuthMode{
+    SIGNED_IN,
+    SIGNED_OUT
+}
+
+enum class SignUpMode{
+    ACTIVE,
+    INACTIVE
 }
